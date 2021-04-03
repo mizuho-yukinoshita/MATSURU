28ccd8f12fb6ec10a32a6b3ddaf17e64b45f0940
GAO RUI <grmagicdog@gmail.com>
2021-04-03 22:27:57 +0900

    fix typo

--- MTSURU/Assets/Scripts/DoorController.cs
+++ MTSURU/Assets/Scripts/DoorController.cs
@@ -32,7 +32,7 @@ public class DoorController : MonoBehaviour
     void Update()
     {
         Move();
-        checkGameOver();
+        CheckGameOver();
     }
 
     void Move()
@@ -49,7 +49,7 @@ public class DoorController : MonoBehaviour
         }
     }
 
-    void checkGameOver()
+    void CheckGameOver()
     {
         if (openDuration > TIMEOUT)
         {
